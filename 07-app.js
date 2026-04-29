5efc327e204f5e834a069357d6ea531f1ed735b3
Rodrigo Lourenço <rodrigo.lourenco@telefonica.com>
2026-04-29 19:08:53 -0300

    renomeando arquivos

---
+++ 07-app.js
@@ -0,0 +1,12 @@
+let nome = "Rodrigo";
+let arr = ["Black", "Rodrigo", "Negão"]; //Array
+console.log(nome[3]);
+console.log(arr[1]);
+
+for(let i = 0; i < nome.length; i++){
+    console.log(nome[i]);
+}
+
+for(let i = 0; i < arr.length; i++){
+    console.log(arr[i]);
+}
--- expl01.js
+++
@@ -1,5 +0,0 @@
-let nome = ["Black", "Rodrigo", "Negão"];
-console.log(nome[3]);
-for(let i = 0; i < nome.length; i++){
-    console.log(nome[i]);
-}
